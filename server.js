1b683f5969e410ad04c048b7f4e291f00d1ff75f
dangtuananhbk55-code <dangtuananhbk55@gmail.com>
2026-08-29 00:18:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const http = require("http");
-const os = require("os");
+const path = require("path");
 const { Server } = require("socket.io");
 const QRCode = require("qrcode");
 
@@ -9,46 +9,44 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
-
 let state = { yesCount: 0, noClicks: 0 };
 
 function broadcast() {
   io.emit("state", state);
 }
 
-function getLocalIPv4() {
-  const nets = os.networkInterfaces();
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      if (net.family === "IPv4" && !net.internal) {
-        return net.address;
-      }
-    }
-  }
-  return "localhost";
-}
-
-app.get("/info", (req, res) => {
-  const ip = getLocalIPv4();
-  const port = process.env.PORT || 3000;
-  res.json({
-    playerUrl: `http://${ip}:${port}/`,
-    hostUrl: `http://${ip}:${port}/host`
-  });
+// Trang người chơi
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Màn hình máy chiếu
+app.get("/host", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "host.html"));
+});
+
+// QR luôn trỏ về trang người chơi công khai
 app.get("/qr", async (req, res) => {
   try {
-    const protocol = req.headers["x-forwarded-proto"] || req.protocol;
-    const host = req.get("host");
+    const protocol = req.headers["x-forwarded-proto"] || req.protocol || "https";
+    const host = req.headers["x-forwarded-host"] || req.get("host");
     const playerUrl = `${protocol}://${host}/`;
-    const png = await QRCode.toBuffer(playerUrl, { width: 600, margin: 2 });
+
+    const png = await QRCode.toBuffer(playerUrl, {
+      width: 700,
+      margin: 2,
+      errorCorrectionLevel: "M"
+    });
+
+    res.setHeader("Cache-Control", "no-store");
     res.type("png").send(png);
   } catch (err) {
-    res.status(500).send("QR error");
+    console.error("QR ERROR:", err);
+    res.status(500).send("Không tạo được QR");
   }
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   socket.emit("state", state);
@@ -71,14 +69,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
-  const ip = getLocalIPv4();
-  console.log("");
-  console.log("==============================================");
-  console.log("  MINI-GAME QR - THAY TUAN ANH");
-  console.log("==============================================");
-  console.log(`Trang người chơi : http://${ip}:${PORT}/`);
-  console.log(`Màn hình máy chiếu: http://${ip}:${PORT}/host`);
-  console.log("==============================================");
-  console.log("Máy chiếu và điện thoại phải cùng Wi-Fi.");
-  console.log("");
+  console.log(`Server running on port ${PORT}`);
 });
